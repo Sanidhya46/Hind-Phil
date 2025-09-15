78e2493dd6e36b3488e541234e6aa87782f64268
Sanidhya Shukla <168342496+Sanidhya46@users.noreply.github.com>
2025-09-15 23:34:36 +0530

    Update index.js

--- HindPhil-backend/index.js
+++ HindPhil-backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hind Phil Backend');
+    res.send('Hind Phil Backend');            
 });
 
 app.listen(PORT, () => {
